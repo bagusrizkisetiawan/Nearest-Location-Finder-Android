fb293eeaa01aadc69a92d6643d5ee2d8ffe07e00
Bagus Rizki Setiawan <bagus30122002@gmail.com>
2024-06-27 17:12:50 +0700

    upload src

---
+++ app/src/main/java/com/bagusrizki/ainun/ui/StokDarahActivity.kt
@@ -0,0 +1,21 @@
+package com.bagusrizki.ainun.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bagusrizki.ainun.R
+
+class StokDarahActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_stok_darah)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
